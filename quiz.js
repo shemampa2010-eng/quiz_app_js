1a190819311fcde23d3cd8c90bb58adde513f965
Paulin <shemampa2010@gmail.com>
2026-08-06 11:34:02 +0200

    changed the style after a click on the quiz

--- quiz.js
+++ quiz.js
@@ -2,4 +2,6 @@ let question = document.getElementById("question");
 
 question.addEventListener("click", function(){
     question.textContent = "Kigali";
+    question.style.backgroundColor = "green"
+    question.style.color = "white"
 });
