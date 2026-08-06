10958f1979270532c21f5969e86fe79e2e20427e
Paulin <shemampa2010@gmail.com>
2026-08-06 11:55:55 +0200

    change the class onclick

--- quiz.js
+++ quiz.js
@@ -1,7 +1,6 @@
 let question = document.getElementById("question");
 
 question.addEventListener("click", function(){
+    question.classList.add('active')
     question.textContent = "Kigali";
-    question.style.backgroundColor = "green"
-    question.style.color = "white"
 });
